1b6bcccfb7d6cd30a1a36fc827cdd5a3bfa812d7
dmcinducil <dominiqueinducil@gmail.com>
2025-02-11 11:10:29 +0800

    Update steamlit_app.py
    Added import requests

--- steamlit_app.py
+++ steamlit_app.py
@@ -29,7 +29,6 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string  += fruit_chosen + ' '
         
-
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -45,3 +44,6 @@ if ingredients_list:
         
         st.success("Your Smoothie is ordered, " + name_on_order +"!", icon="✅")
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
